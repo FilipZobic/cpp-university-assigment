e1b8ebb42d96f50a9ce13c2de3e51ebfabdf6bac
FilipZobic <zobicfilip919@gmail.com>
2021-01-12 14:40:28 +0100

    Finish Department GUI

---
+++ gui/DepartmentGUI/BusinessDisplayModel.h
@@ -0,0 +1,30 @@
+//
+// Created by filipz on 1/11/21.
+//
+
+#ifndef CPP_UNIVERSITY_ASSIGMENT_BUSINESSDISPLAYMODEL_H
+#define CPP_UNIVERSITY_ASSIGMENT_BUSINESSDISPLAYMODEL_H
+
+#include <gui/AbstractDisplay/AbstractNavigationDisplayModel.h>
+#include <model/Business.h>
+
+class BusinessDisplayModel : public AbstractNavigationDisplayModel<Business*> {
+protected:
+    Fl_Output *a1;
+    Fl_Output *a2;
+    Fl_Output *a3;
+    Fl_Output *a4;
+    Fl_Output *a5;
+    Fl_Output *a6;
+public:
+    BusinessDisplayModel(Business *currentEntity, vector<Business*> *entities);
+
+    Fl_Group *createDetailsDisplay() override;
+
+    string getMainDisplayText() override;
+
+    void updateDetailDisplay() override;
+};
+
+
+#endif //CPP_UNIVERSITY_ASSIGMENT_BUSINESSDISPLAYMODEL_H
